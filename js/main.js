b3e9e43be04620c7a0bce21cc9bc92bce44c2486
Lukasz Stank <lukasz.stank@gmail.com>
2020-02-26 20:25:11 +0100

    thumbs up / down handlers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// SECTION 1 - input-option
-
 const inputNumber = document.getElementById('input-number');
 const inputArrow = [...document.querySelectorAll('.input-arrow')];
 const inputArrowUp = [...document.querySelectorAll('.input-option__arrow--up')];
@@ -36,6 +34,128 @@ function handleArrowDown() {
 };
 
 
+class Box {
+  constructor(idUp, idDown) {
+    this.idUp = idUp;
+    this.idDown = idDown;
+    this.upValue = 0;
+    this.downValue = 0;
+    this.avg = 0;
+  }
+}
+
+const box1 = new Box("1-up", "1-down");
+
+const box2 = new Box("2-up", "2-down");
+
+const box3 = new Box("3-up", "3-down");
+
+function math(item) {
+  return Math.floor(item * 100)
+}
+
+function avg(box) {
+  box.avg = box.upValue / (box.upValue + box.downValue);
+}
+
+
+// THUMBS UP FUNCTION
+
+function thumbUp() {
+
+  upCounter.forEach(item => {
+
+    if (this.id === item.dataset.thumbup) {
+
+      if (this.id === box1.idUp) {
+        box1.upValue++;
+        item.textContent = box1.upValue;
+        avg(box1);
+      }
+
+      if (this.id === box2.idUp) {
+        box2.upValue++;
+        item.textContent = box2.upValue;
+        avg(box2)
+      }
+
+      if (this.id === box3.idUp) {
+        box3.upValue++;
+        item.textContent = box3.upValue;
+        avg(box3);
+      }
+    }
+  });
+
+  // Avarage 
+  reviewAvg.forEach(item => {
+
+    if (this.id === item.dataset.thumbup) {
+      if (this.id === box1.idUp) {
+
+        item.textContent = math(box1.avg);
+      }
+      if (this.id === box2.idUp) {
+
+        item.textContent = math(box2.avg);
+      }
+      if (this.id === box3.idUp) {
+
+        item.textContent = math(box3.avg);
+      }
+    }
+  })
+};
+
+// THUMBS DOWN FUNCTION
+
+function thumbDown() {
+
+  downCounter.forEach(item => {
+
+    if (this.id === item.dataset.thumbdown) {
+
+      if (this.id === box1.idDown) {
+        box1.downValue++;
+        item.textContent = box1.downValue;
+        avg(box1);
+      }
+
+      if (this.id === box2.idDown) {
+        box2.downValue++;
+        item.textContent = box2.downValue;
+        avg(box2);
+      }
+
+      if (this.id === box3.idDown) {
+        box3.downValue++;
+        item.textContent = box3.downValue;
+        avg(box3);
+      }
+    }
+  });
+
+  reviewAvg.forEach(item => {
+
+    if (this.id === item.dataset.thumbdown) {
+      if (this.id === box1.idDown) {
+
+        item.textContent = math(box1.avg);
+      }
+      if (this.id === box2.idDown) {
+
+        item.textContent = math(box2.avg);
+      }
+      if (this.id === box3.idDown) {
+
+        item.textContent = math(box3.avg);
+      }
+    }
+
+  })
+};
+
+
 inputArrowUp.forEach(arrow => arrow.addEventListener('click', handleArrowUp));
 
 inputArrowDown.forEach(arrow => arrow.addEventListener('click', handleArrowDown));
@@ -54,23 +174,6 @@ inputArrow.forEach(arrow => arrow.addEventListener('mouseup', function () {
 
 // Thumbs up/down handler
 
+thumbsUp.forEach(item => item.addEventListener('click', thumbUp));
 
-thumbsUp.forEach(item => item.addEventListener('click', function () {
-
-  // Thumb-up addition
-  upCounter.forEach(item => {
-
-    if (this.id === item.dataset.thumb) {
-      item.textContent++;
-    }
-
-  });
-
-  // Avaragre 
-  reviewAvg.forEach(item => {
-
-    if (this.id === item.dataset.thumb)
-      item.textContent++
-
-  })
-}))
+thumbsDown.forEach(item => item.addEventListener('click', thumbDown));
